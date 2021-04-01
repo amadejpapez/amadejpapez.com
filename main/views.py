1e2a91d94ec4a1f6545cca52ee8307b29277d00d
amadejpapez <amadej.papez@gmail.com>
2021-04-01 17:59:15 +0200

    Added new About Me page, moved projects to 'projects/'

--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def main_view(request, *args, **kwargs):
         return render(request, "main.html", {})
+
+def projects_view(request, *args, **kwargs):
+        return render(request, "projects.html", {})
--- website/urls.py
+++ website/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin
 from django.urls import path
-from main.views import main_view
-
+from main.views import main_view, projects_view
 
 urlpatterns = [
     path("", main_view),
     path("admin/", admin.site.urls),
+    path("projects/", projects_view)
 ]
