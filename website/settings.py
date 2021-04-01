1140089b5c17adb65751ae023d1830ca14c6c079
amadejpapez <amadej.papez@gmail.com>
2021-04-01 13:53:50 +0200

    Fixed static files with debug turned off

--- website/settings.py
+++ website/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'website.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, '/main/templates'),
+        'DIRS': os.path.join(os.path.dirname(BASE_DIR), 'templates'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,3 +122,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_URL = "/static/"
+STATICFILES_DIRS = [os.path.join(os.path.dirname(BASE_DIR), "static"),]
