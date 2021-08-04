35105862137fd231919eaab6db70d6bbc97d1051
amadejpapez <amadej.papez@icloud.com>
2021-08-04 18:55:14 +0200

    feat: hide nav bar on scroll and more mobile tweaks
    - navigation bar disappears when scrolling down and appears on scrolling up
    - tweaked sizing numbers to look better on mobile
    - updated wsgi.py file according to the needs of pythonanywhere

--- django_website/wsgi.py
+++ django_website/wsgi.py
@@ -1,16 +1,31 @@
-"""
-WSGI config for website project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
 import os
-
+import sys
+import dotenv
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_website.settings')
+## assuming your django settings file is at '/home/amadejpapez/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/amadejpapez/mysite/manage.py'
+path = "/home/amadejpapez/website"
+if path not in sys.path:
+    sys.path.append(path)
 
-application = get_wsgi_application()
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_website.settings')
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
